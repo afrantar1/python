f1f9a9a79955af5bab40bfdc8cffb559f472b71b
afrantar1 <afrantar2@gmail.com>
2021-04-08 17:25:35 +0200

    commit

--- Python/09_Condiitions and loops.py
+++ Python/09_Condiitions and loops.py
@@ -1,16 +1,19 @@
-secret = 22
-guess = 0
+#secret = 22
+#guess = 0
 
-did_guess_secret = False
-
-while not did_guess_secret: #True
+#did_guess_secret = False
+friends = ["Anja","Marko","Janko","Mara","Tjaša"]
+numbers = range(5) #je enako če bi napisali array
+for friend in friends:
+    print(friend+ " is my friend.")
+#while not did_guess_secret: #True
     #Do all this
-    guess = int(input("Guess the secret number (between 1 and 30): "))
-    did_not_guess_secret = guess == secret
-    if guess == secret:
-        print("You guessed it - congratulations! It's number 22.")
-    else:
-        print("Sorry, your guess is not correct... The secret number is not " + str(guess))
+    #guess = int(input("Guess the secret number (between 1 and 30): "))
+    #did_not_guess_secret = guess == secret
+    #if guess == secret:
+        #print("You guessed it - congratulations! It's number 22.")
+    #else:
+        #print("Sorry, your guess is not correct... The secret number is not " + str(guess))
 
 print("End")
 

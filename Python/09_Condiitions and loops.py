3f39ad0c93b338d4a0fe9085e9c332e2c06a4f46
afrantar1 <afrantar2@gmail.com>
2021-04-13 17:39:40 +0200

    add read/write examples

--- Python/09_Condiitions and loops.py
+++ Python/09_Condiitions and loops.py
@@ -1,19 +1,17 @@
-#secret = 22
-#guess = 0
+import random
+secret = random.randint(1,30)
+guess = None
 
-#did_guess_secret = False
-friends = ["Anja","Marko","Janko","Mara","Tjaša"]
-numbers = range(5) #je enako če bi napisali array
-for friend in friends:
-    print(friend+ " is my friend.")
-#while not did_guess_secret: #True
-    #Do all this
-    #guess = int(input("Guess the secret number (between 1 and 30): "))
-    #did_not_guess_secret = guess == secret
-    #if guess == secret:
-        #print("You guessed it - congratulations! It's number 22.")
-    #else:
-        #print("Sorry, your guess is not correct... The secret number is not " + str(guess))
+did_guess_secret = False
 
-print("End")
+while  not did_guess_secret: #True
+
+    guess = int(input("Guess the secret number (between 1 and 30): "))
+    id_not_guess_secret = guess == secret
+    did_guess_secret = guess == secret
+    if did_guess_secret:
+        print("You guessed it - congratulations! It's number 22." + str(secret))
+        break
+
+
 

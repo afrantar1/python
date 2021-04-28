030aeef344fbb5bbb5829b0bb3e300fcbd441515
afrantar1 <afrantar2@gmail.com>
2021-04-28 09:42:29 +0200

    Add example lists and dictionaries

---
+++ Python/List and dictionary.py
@@ -0,0 +1,27 @@
+grades = [1, 2, 3, 4, 5]
+
+
+city_names = ["Ljubljana", "Maribor", "New York", "London"]
+
+capita_of_slovenia = {"name": "Ljubljana", "population": 200000, "crime_trate": "low"}
+
+capita_of_england = {"name": "London", "population": 1000000, "crime_trate": "moderate"}
+
+capitals = [{"name": "Ljubljana",
+             "population": 200000,
+             "crime_trate": "low"
+             },
+             {"name": "London",
+             "population": 1000000,
+             "crime_trate": "moderate"
+              }
+            ]
+capitals1 = [capita_of_slovenia, capita_of_england]
+
+my_book = {"title": "Kako izgoreti", "pages": 200, "author": "Nekdo"}
+
+my_fruit= {
+    "type": "apple",
+    "color": "red",
+    "weight":100
+}
